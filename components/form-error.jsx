ab3786ebcea011e9cc8005c9aa022e81718d268e
miquelven <miquelven.silva@gmail.com>
2024-07-20 10:56:38 -0300

    feat: add verification for show message

--- components/form-error.jsx
+++ components/form-error.jsx
@@ -2,9 +2,13 @@ import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
 
 export default function FormError({ message }) {
   return (
-    <div className="bg-destructive/15 rouded-md p-3 flex items-center gap-x-2 text-sm text-destructive">
-      <ExclamationTriangleIcon className="w-4 h-4" />
-      <p>{message}</p>
-    </div>
+    <>
+      {message && (
+        <div className="bg-destructive/15 rouded-md p-3 flex items-center gap-x-2 text-sm text-destructive">
+          <ExclamationTriangleIcon className="w-4 h-4" />
+          <p>{message}</p>
+        </div>
+      )}
+    </>
   );
 }
--- components/form-success.jsx
+++ components/form-success.jsx
@@ -2,9 +2,13 @@ import { CheckCircledIcon } from "@radix-ui/react-icons";
 
 export default function FormSuccess({ message }) {
   return (
-    <div className="bg-emerald-500/15 rouded-md p-3 flex items-center gap-x-2 text-sm text-emerald-500">
-      <CheckCircledIcon className="w-4 h-4" />
-      <p>{message}</p>
-    </div>
+    <>
+      {message && (
+        <div className="bg-emerald-500/15 rouded-md p-3 flex items-center gap-x-2 text-sm text-emerald-500">
+          <CheckCircledIcon className="w-4 h-4" />
+          <p>{message}</p>
+        </div>
+      )}
+    </>
   );
 }
