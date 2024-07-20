6b571ed974faf6d54209bd60f629d1555c7c4d1e
miquelven <miquelven.silva@gmail.com>
2024-07-20 19:43:28 -0300

    feat: add signin callback

--- auth.js
+++ auth.js
@@ -4,6 +4,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
 
 import authConfig from "@/auth.config";
+import { getUserById } from "./data/user";
 
 export const {
   handlers: { GET, POST },
@@ -11,6 +12,20 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider !== "credentials") return true;
+
+      const existingUser = await getUserById(user.id);
+
+      if (!existingUser?.emailVerified) return false;
+
+      return true;
+    },
+    async jwt({ token }) {
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
