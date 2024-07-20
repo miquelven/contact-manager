8327bdea474cb84039954081cb817492727e255d
miquelven <miquelven.silva@gmail.com>
2024-07-20 13:21:43 -0300

    feat: add feature for add user

--- actions/register.js
+++ actions/register.js
@@ -1,6 +1,11 @@
 "use server";
 
+import bcrypt from "bcryptjs";
+
+import { db } from "@/lib/db";
+
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -9,5 +14,23 @@ export const register = async (values) => {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "User created!" };
 };
