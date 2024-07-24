f19f2abf375e7b1e196e808007b553e6e8fc737e
miquelven <miquelven.silva@gmail.com>
2024-07-24 17:27:56 -0300

    feat: create editpage page

---
+++ front/app/(protected)/contact/edit-user/page.jsx
@@ -0,0 +1,21 @@
+import Header from "@/components/auth/header";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { auth } from "@/auth";
+import InfoUserArea from "@/components/userArea/info-user-area";
+
+export default async function EditUserPage() {
+  const session = await auth();
+
+  return (
+    <div className="w-[500px] mx-auto flex justify-center items-center">
+      <Card className="w-[300px] shadow-md sm:w-full">
+        <CardHeader>
+          <Header title="Suas Informações" />
+        </CardHeader>
+        <CardContent>
+          <InfoUserArea data={session.user} />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
