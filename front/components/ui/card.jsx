dc85fabbf246ed353c7cbdae25e76035a47c76c2
miquelven <miquelven.silva@gmail.com>
2024-07-24 17:25:12 -0300

    style: add shadow color

--- front/components/ui/card.jsx
+++ front/components/ui/card.jsx
@@ -6,7 +6,7 @@ const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn(
-      "rounded-lg  bg-card text-card-foreground shadow-md ",
+      "rounded-lg  bg-card text-card-foreground shadow-md shadow-black/30 ",
       className
     )}
     {...props}
