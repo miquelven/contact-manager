b0c58b158879baefeea00dbdd549d4cfa41468bd
miquelven <miquelven.silva@gmail.com>
2024-07-24 17:24:08 -0300

    feat: create form location component

---
+++ front/components/userArea/form-location.jsx
@@ -0,0 +1,24 @@
+const LocationItem = ({ label, value }) => (
+  <li className="flex items-center gap-3">
+    <h4 className="font-medium text-lg">{label}:</h4>
+    {value ? (
+      <span className="text-light">{value}</span>
+    ) : (
+      <span className="text-light">Não há dados salvos</span>
+    )}
+  </li>
+);
+
+export default function FormLocation({ data }) {
+  const locationData = data[0] || {};
+
+  return (
+    <ul className="flex flex-col gap-4">
+      <LocationItem label="Cidade" value={locationData.city} />
+      <LocationItem label="Rua" value={locationData.street} />
+      <LocationItem label="Bairro" value={locationData.neighborhood} />
+      <LocationItem label="Número" value={locationData.number} />
+      <LocationItem label="CEP" value={locationData.zipCode} />
+    </ul>
+  );
+}
