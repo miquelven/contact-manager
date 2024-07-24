490a330f734396dd82d63c04c502e5cd839ec3fa
miquelven <miquelven.silva@gmail.com>
2024-07-24 17:30:06 -0300

    refactor: add userouteslocation route

--- back/index.js
+++ back/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import useRoutes from "./routes/contacts.js";
+import useRoutesLocation from "./routes/locations.js";
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/", useRoutes);
+app.use("/", useRoutesLocation);
 
 app.listen(8800);
