09b627c99542cd79a0f302e455305b9da3413292
miquelven <miquelven.silva@gmail.com>
2024-07-24 17:28:30 -0300

    feat: add layout contact

---
+++ front/app/(protected)/contact/layout.jsx
@@ -0,0 +1,7 @@
+export default function ContactLayout({ children }) {
+  return (
+    <div className="h-screen w-full mx-auto max-w-screen-xl flex justify-center items-center">
+      {children}
+    </div>
+  );
+}
