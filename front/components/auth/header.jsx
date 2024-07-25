fb074801f53d0c6e976ca06f27ee4f5dc9d6a8a9
miquelven <miquelven.silva@gmail.com>
2024-07-25 05:28:21 -0300

    feat: add toast

--- front/components/auth/header.jsx
+++ front/components/auth/header.jsx
@@ -9,7 +9,10 @@ const font = Poppins({
 
 export default function Header({ label, title }) {
   return (
-    <div className="w-full flex flex-col gap-y-4 items-center justify-center">
+    <div
+      data-aos="fade-up"
+      className="w-full flex flex-col gap-y-4 items-center justify-center"
+    >
       <h1 className={cn("text-3xl font-semibold", font.className)}>{title}</h1>
       <p className="text-muted-foreground text-sm">{label}</p>
     </div>
