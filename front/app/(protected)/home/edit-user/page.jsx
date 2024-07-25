d6117ebbfebbe1e2f8ce4617098fba045137d2e9
miquelven <miquelven.silva@gmail.com>
2024-07-25 04:21:32 -0300

    style: center in the middle

--- front/app/(protected)/home/edit-user/page.jsx
+++ front/app/(protected)/home/edit-user/page.jsx
@@ -7,15 +7,17 @@ export default async function EditUserPage() {
   const session = await auth();
 
   return (
-    <div className="w-[500px] mx-auto flex justify-center items-center">
-      <Card className="w-[300px] shadow-md sm:w-full">
-        <CardHeader>
-          <Header title="Suas Informações" />
-        </CardHeader>
-        <CardContent>
-          <InfoUserArea data={session.user} />
-        </CardContent>
-      </Card>
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="w-[500px] mx-auto flex justify-center items-center">
+        <Card className="w-[300px] shadow-md sm:w-full">
+          <CardHeader>
+            <Header title="Suas Informações" />
+          </CardHeader>
+          <CardContent>
+            <InfoUserArea data={session.user} />
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
