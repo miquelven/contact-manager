5e9fa336caf9052c2d0295e43a33289a3303cc67
miquelven <miquelven.silva@gmail.com>
2024-07-24 23:36:55 -0300

    style: change background color

--- front/app/(protected)/contact/layout.jsx
+++ front/app/(protected)/contact/layout.jsx
@@ -1,7 +1,3 @@
 export default function ContactLayout({ children }) {
-  return (
-    <div className="h-screen w-full mx-auto max-w-screen-xl flex justify-center items-center">
-      {children}
-    </div>
-  );
+  return <div className=" w-full  bg-white min-h-screen">{children}</div>;
 }
