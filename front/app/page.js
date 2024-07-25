510f0a40afc649b584550b3436f1907c15405394
miquelven <miquelven.silva@gmail.com>
2024-07-25 10:59:16 -0300

    fix: deploy error

--- front/app/page.js
+++ front/app/page.js
@@ -1,14 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/auth/login");
-  }, [router]);
-
-  return null;
+  return <></>;
 }
