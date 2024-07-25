377c08d9cf13ac0967c6fe7b9361d41c36d26057
miquelven <miquelven.silva@gmail.com>
2024-07-25 10:53:48 -0300

    feat: redirect for login

--- front/app/page.js
+++ front/app/page.js
@@ -1,3 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Home() {
-  return <></>;
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/auth/login");
+  }, [router]);
+
+  return null;
 }
