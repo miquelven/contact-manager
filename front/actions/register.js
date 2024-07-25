d4890b51822ed3b170c863a3c579e66ba29df74e
miquelven <miquelven.silva@gmail.com>
2024-07-24 22:02:44 -0300

    refactor: add logs

--- front/actions/register.js
+++ front/actions/register.js
@@ -1,42 +1,48 @@
 "use server";
 
 import bcrypt from "bcryptjs";
-
 import { db } from "@/lib/db";
-
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import { generateVerificationToken } from "@/lib/tokens";
 import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values) => {
+  console.log("Register function called with values:", values);
+
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
+    console.error("Validation failed:", validatedFields.error);
     return { error: "Invalid fields" };
   }
 
   const { email, password, name } = validatedFields.data;
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const existingUser = await getUserByEmail(email);
 
-  const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+      console.error("Email already in use:", email);
+      return { error: "Email already in use!" };
+    }
 
-  if (existingUser) {
-    return { error: "Email already in use!" };
+    await db.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const verificationToken = await generateVerificationToken(email);
+    await sendVerificationEmail(email, verificationToken.token);
+
+    console.log("Registration successful, confirmation email sent.");
+    return { success: "Confirmation email sent!" };
+  } catch (error) {
+    console.error("Error during registration:", error);
+    return { error: "Registration failed!" };
   }
-
-  await db.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  const verificationToken = await generateVerificationToken(email);
-
-  await sendVerificationEmail(verificationToken.email, verificationToken.token);
-
-  return { success: "Confirmation email sent!" };
 };
